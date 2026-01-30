5bc18a156f9c1753627c5dfb3acfee341a75b22a
Ivan Vergés <ivan@pokecode.net>
2026-01-30 11:17:41 +0100

    fix css for SSO. Remove unnecessary files

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
-require "sidekiq/web"
-require "sidekiq/cron/web"
-
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  authenticate :user, ->(u) { u.admin? } do
-    mount Sidekiq::Web => "/sidekiq"
-  end
 
   mount Decidim::Core::Engine => "/"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
